2fe03aed0db76e77d8495daa4459e14475d705b6
cctakaet <44292656+cctakaet@users.noreply.github.com>
2020-01-16 17:06:10 +0800

    Update weight_result.sh

--- weight_result.sh
+++ weight_result.sh
@@ -3,3 +3,5 @@ mkdir result/Synthetic
 mkdir result/Real
 mkdir checkpoint
 wget --no-check-certificate "https://onedrive.live.com/download?cid=F80778D80179676E&resid=F80778D80179676E%211349&authkey=AJOnXyDTUNZTeXI" -O checkpoint/kitti2015_final.pth
+apt-get update
+apt-get install -y libsm6 libxext6 libxrender-dev
