fbb109eabedd4dd13521a236ee6a62b212781b3e
cctakaet <44292656+cctakaet@users.noreply.github.com>
2020-01-16 14:11:00 +0800

    Create weight_result.sh

---
+++ weight_result.sh
@@ -0,0 +1,5 @@
+mkdir result
+mkdir result/Synthetic
+mkdir result/Real
+mkdir checkpoint
+wget --no-check-certificate "https://onedrive.live.com/download?cid=F80778D80179676E&resid=F80778D80179676E%211349&authkey=AJOnXyDTUNZTeXI" -O checkpoint/kitti2015_final.pth
